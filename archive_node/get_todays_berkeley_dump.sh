e7428c5bc4bc474437b6bcb2b601da88996d3b23
Isaac <isaac@granola.team>
2023-02-21 07:06:59 +0000

    Update get today's dump

--- archive_node/get_todays_berkeley_dump.sh
+++ archive_node/get_todays_berkeley_dump.sh
@@ -1,3 +1,23 @@
-# mina-archive-dumps/berkeley-archive-dump-2023-02-21_0000.sql.tar.gz
+# Download today's Berkeley archive dump
+
+dumps=""
+
+for item in *; do
+  if [[ -d $item ]]; then
+    if [[ $item == "dumps" ]]; then
+      dumps+=0;
+      break;
+    fi
+  fi
+done
+
+if [[ $dumps == "" ]]; then
+  mkdir ./dumps;
+fi
+
+cd dumps
+
 _date=$(date -I)
 gsutil cp -n "gs://mina-archive-dumps/berkeley-archive-dump-${_date}*.sql.tar.gz" .
+
+cd ..
