37da9d0d5a8e0220a94a6eedb7f1f6f5c54c96c2
Quantifier <isaacdefrain@gmail.com>
2023-02-21 19:57:13 -0500

    Add user messages to build_archive.sh

--- archive_node/build_with_nix/build_archive.sh
+++ archive_node/build_with_nix/build_archive.sh
@@ -1,13 +1,24 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Run from the Mina project root after
-# installing nix and setting up mina in the registry
+# Run from the Mina project root
+
+# 1. install nix
+# 2. set up mina in nix registry
 
 # start nix dev shell for mina
 nix develop mina
 
-# build mina.exe
+# build
+echo "Building Mina daemon..."
 dune build src/app/cli/src/mina.exe
 
-# build archive.exe
+echo "Building archive process..."
 dune build src/app/archive/archive.exe
+
+DIR_NAME=$( dirname -- "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$( cd -- $DIR_NAME &> /dev/null && pwd )
+
+# suggest
+echo "Success!";
+echo "  To give friendlier names to the executables, do"
+echo "  $ source ${SCRIPT_DIR}/.env";
